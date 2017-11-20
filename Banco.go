13fc97224528b4cd5f9e1b4e3859198b8d3c6a7f
JCaymme <jean26121996@gmail.com>
2017-11-20 15:14:54 -0200

    Projeto

---
+++ Banco.go
@@ -0,0 +1,37 @@
+package main 
+
+import "fmt"
+
+type Banco struct{
+	Nome string
+	Saldo float32
+}
+func (b *Banco) Sacar(valor float32) float32{
+	b.Saldo = b.Saldo - valor
+	return b.Saldo
+}
+func (b *Banco) Depositar(valor float32) float32{
+	b.Saldo = b.Saldo + valor
+	return b.Saldo
+}
+func (b *Banco) TransferePara(valor float32, destino *Banco)(float32, float32){
+	b.Saldo = b.Saldo - valor
+	destino.Saldo = destino.Saldo + valor
+	return b.Saldo, destino.Saldo
+}
+func main(){
+	j := Banco{Nome: "Jean Caymme", Saldo: 2000}
+	a := Banco{Nome: "Arlete", Saldo: 3000}
+	
+	fmt.Println("Saldo de "+j.Nome+": ",j.Saldo)
+	fmt.Println("Saldo de "+a.Nome+": ",a.Saldo)
+
+	j.TransferePara(4000, &a)
+	fmt.Println("---------- Transferencia ----------")
+	fmt.Println("Saldo de "+j.Nome+": ",j.Saldo)
+	fmt.Println("Saldo de "+a.Nome+": ",a.Saldo)
+
+	//m.Sacar(300)
+	//m.Depositar(500)
+	//fmt.Println(m.Saldo)
+}
